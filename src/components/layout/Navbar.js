6640154815267bbf7da090310be8af687388e093
Srikar Reddy <saisrikarreddy101@gmail.com>
2021-06-08 20:58:08 -0500

    final commit

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -25,9 +25,4 @@ Navbar.defaultProps = {
   icon: 'fab fa-github',
 }
 
-// Navbar.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   icon: PropTypes.string.isRequired,
-// }
-
 export default Navbar
